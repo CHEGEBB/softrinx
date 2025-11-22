c89270e03cd35ee6756b0e7fc7ef2ef4ac99c104
CHEGEBB <chegephil24@gmail.com>
2025-11-22 13:31:02 +0300

    Add About page, Header, and Footer components; update global styles

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Aboutpage() {
+  return (
+    <div className='bg-red-400'>
+        <h1 className='text-2xl text-white'>About Page</h1>
+        <p>This is the about page of our Next.js application.</p>
+      
+    </div>
+  )
+}
+
+export default Aboutpage
---
+++ components/layout/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <h1 className='text-3xl font-bold underline'>
+            Header Component
+            </h1>
+      
+    </div>
+  )
+}
+
+export default Header
